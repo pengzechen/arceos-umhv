4337ad5d9bafa5337780548f4da07e6dd4294c1e
ajax <927068267@qq.com>
2024-08-21 09:47:36 +0800

    tmp

--- crates/axvm/src/arch/aarch64/device_list.rs
+++ crates/axvm/src/arch/aarch64/device_list.rs
@@ -1,3 +1,5 @@
+use core::marker::PhantomData;
+use alloc::vec::Vec;
 use axerrno::AxResult;
 
 use super::AxArchVCpuImpl;
@@ -9,8 +11,17 @@ pub struct AxArchDeviceList<H: AxVMHal> {
 
 impl<H: AxVMHal> AxArchDeviceList<H> {
     pub fn new() -> Self {
+        let mut arch_vcpu_list = Vec::new();
+
+        for arch_vcpu in list {
+            let unsafe_cell_vcpu = unsafe { &*arch_vcpu.arch_vcpu.get() };
+            let vcpu = unsafe_cell_vcpu.clone().into();
+            arch_vcpu_list.push(vcpu);
+        }
+
         Self {
-            _marker: core::marker::PhantomData,
+            vgic: emu_intc_init(0x800_0000, 0x4_0000, &arch_vcpu_list),
+            _marker: PhantomData,
         }
     }
 
